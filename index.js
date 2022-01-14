b959b6abd01fced543572f84b7da0873fb595fe2
Matheus <matheusphilippe.mallet@gmail.com>
2022-01-14 09:27:30 -0300

    updating a app as test

---
+++ index.js
@@ -0,0 +1,47 @@
+const questions = [
+    "Qual o seu nome?",
+    "O que aprendi hoje?",
+    "O que me deixou aborrecido?",
+    "O que eu poderia fazer para melhorar?",
+    "O que me deixou feliz hoje?",
+    "Quantas pessoas ajudei hoje",
+]
+
+const ask =( index = 0 )=> {
+    process.stdout.write("\n\n" + questions[index] + " > ")
+}
+
+ask()
+
+const answers = []
+process.stdin.on('data', data =>{
+    answers.push(data.toString().trim())
+    if(answers.length < questions.length){
+        ask(answers.length)
+    } else{
+        process.exit()
+    }
+
+})
+
+process.on('exit', () =>{
+    console.log(`
+        Bacana ${answers[0]}!
+        
+        O que você aprendeu hoje foi:
+        ${answers[1]})
+
+        O que  te aborreceu hoje foi:
+        ${answers[2]}
+
+        O que você pode melhorar foi:
+        ${answers[3]}
+
+        O que te deixou feliz hoje foi:
+        ${answers[4]}
+
+        Você ajudou ${answers[5]} pessoas hoje!
+
+        Volte amanhã para mais reflexões!
+    `)
+})
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Best me App
+Encontre sua melhor versão!
+
+Use o App ao final de cada dia para refletir e ponderar-se sobre sua jornada.
